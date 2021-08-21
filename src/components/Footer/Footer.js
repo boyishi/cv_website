a939c029e285e2bdd6de496b8dfbe88c47534c56
Adrian Shi <adrian.boyishi@gmail.com>
2021-08-21 11:26:25 -0500

    Added

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from "react-icons/ai";
+
+import { SocialIcons } from "../Header/HeaderStyles";
+import {
+    CompanyContainer,
+    FooterWrapper,
+    LinkColumn,
+    LinkItem,
+    LinkList,
+    LinkTitle,
+    Slogan,
+    SocialContainer,
+    SocialIconsContainer,
+} from "./FooterStyles";
+
+const Footer = () => {
+    return (
+        <FooterWrapper>
+            <LinkList>
+                <LinkColumn>
+                    <LinkTitle> Call </LinkTitle>
+                    <LinkItem href="tel: 281-748-2115"> 281-748-2115 </LinkItem>
+                </LinkColumn>
+                <LinkColumn>
+                    <LinkTitle> Email </LinkTitle>
+                    <LinkItem href="mailto:adrian.boyishi@gmail.com"> adrian.boyishi@gmail.com </LinkItem>
+                </LinkColumn>
+            </LinkList>
+        </FooterWrapper>
+    );
+};
+export default Footer;
